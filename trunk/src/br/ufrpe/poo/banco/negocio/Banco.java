b139456724a17819ce8432de40facd43f2b3b764
tgondim <tgondim@c8e0d0bd-fa27-80b3-f37e-bdf94eea5632>
2012-05-23 14:06:36 +0000

    Criação de interface para a fachada Banco (IBanco) Correção de problemas no RepositórioContasArquivoBin Criação do método Banco.getInstance() (Singleton)

--- trunk/src/br/ufrpe/poo/banco/negocio/Banco.java
+++ trunk/src/br/ufrpe/poo/banco/negocio/Banco.java
@@ -1,18 +1,34 @@
 package br.ufrpe.poo.banco.negocio;
 
 import br.ufrpe.poo.banco.dados.RepositorioContas;
+import br.ufrpe.poo.banco.dados.RepositorioContasArquivoBin;
 import br.ufrpe.poo.banco.dados.RepositorioException;
 
-public class Banco {
+public class Banco implements IBanco {
 
 	private RepositorioContas contas;
 	
 	private final double TAXA_RENDIMENTO_POUPANCA = 0.008;
 
-	public Banco(RepositorioContas rep) {
+	private static IBanco instance;
+	
+	static {
+		try {
+			Banco.instance = new Banco(new RepositorioContasArquivoBin());
+		} catch (RepositorioException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	private Banco(RepositorioContas rep) {
 		this.contas = rep;
 	}
 
+	public static IBanco getInstance() {
+		return Banco.instance;
+	}
+	
 	public void cadastrar(ContaAbstrata conta) throws RepositorioException, ContaJaCadastradaException {
 		String numero = conta.getNumero();
 		if (contas.existe(numero)) {
@@ -24,24 +40,36 @@ public class Banco {
 
 	public void creditar(String numero, double valor) throws RepositorioException, ContaNaoEncontradaException{
 		ContaAbstrata c = contas.procurar(numero);
+		if (c == null) {
+			throw new ContaNaoEncontradaException();
+		}
 		c.creditar(valor);
 		contas.atualizar(c);
 	}
 
 	public void debitar(String numero, double valor) throws RepositorioException, ContaNaoEncontradaException, SaldoInsuficienteException {
 		ContaAbstrata c = contas.procurar(numero);
+		if (c == null) {
+			throw new ContaNaoEncontradaException();
+		}
 		c.debitar(valor);
 		contas.atualizar(c);
 	}
 
 	public double getSaldo(String numero) throws RepositorioException, ContaNaoEncontradaException{
 		ContaAbstrata c = contas.procurar(numero);
+		if (c == null) {
+			throw new ContaNaoEncontradaException();
+		}
 		return c.getSaldo();
 	}
 
 	public void transferir(String de, String para, double valor) throws RepositorioException, ContaNaoEncontradaException, SaldoInsuficienteException {
 		ContaAbstrata origem = contas.procurar(de);
 		ContaAbstrata destino = contas.procurar(para);
+		if (origem == null || destino == null) {
+			throw new ContaNaoEncontradaException();
+		}
 		origem.debitar(valor);
 		destino.creditar(valor);
 		contas.atualizar(origem);
@@ -50,6 +78,9 @@ public class Banco {
 	
 	public void renderJuros(String numero) throws RepositorioException, ContaNaoEncontradaException, RenderJurosPoupancaException{
 		ContaAbstrata c = contas.procurar(numero);
+		if (c == null) {
+			throw new ContaNaoEncontradaException();
+		}
 		if (c instanceof Poupanca) {
 			((Poupanca) c).renderJuros(TAXA_RENDIMENTO_POUPANCA);
 			contas.atualizar(c);
@@ -61,6 +92,9 @@ public class Banco {
 
 	public void renderBonus(String numero) throws RepositorioException, ContaNaoEncontradaException, RenderBonusContaEspecialException{
 		ContaAbstrata c = contas.procurar(numero);
+		if (c == null) {
+			throw new ContaNaoEncontradaException();
+		}
 		if (c instanceof ContaEspecial) {
 			((ContaEspecial) c).renderBonus();
 			contas.atualizar(c);
