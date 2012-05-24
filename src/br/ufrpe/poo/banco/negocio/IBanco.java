54896c1e776fc54ba9fb8f481244acceb07c7d11
sidney.ufrpe <sidney.ufrpe@c8e0d0bd-fa27-80b3-f37e-bdf94eea5632>
2012-05-24 14:06:46 +0000

    inserido javadoc

--- src/br/ufrpe/poo/banco/negocio/IBanco.java
+++ src/br/ufrpe/poo/banco/negocio/IBanco.java
@@ -3,18 +3,113 @@ package br.ufrpe.poo.banco.negocio;
 import br.ufrpe.poo.banco.dados.RepositorioException;
 
 public interface IBanco {
-	
-	public void cadastrar(ContaAbstrata conta) throws RepositorioException, ContaJaCadastradaException;
 
-	public void creditar(String numero, double valor) throws RepositorioException, ContaNaoEncontradaException;
+	/**
+	 * Cadatra uma conta no banco.
+	 * 
+	 * @param conta
+	 *            conta a ser cadastrada.
+	 * @throws RepositorioException
+	 *             lancada caso ocorra erro com a persistencia.
+	 * @throws ContaJaCadastradaException
+	 *             lancada caso a conta ja existe no banco.
+	 */
+	void cadastrar(ContaAbstrata conta) throws RepositorioException,
+			ContaJaCadastradaException;
 
-	public void debitar(String numero, double valor) throws RepositorioException, ContaNaoEncontradaException, SaldoInsuficienteException;
+	/**
+	 * Credita valor em uma conta do banco.
+	 * 
+	 * @param numero
+	 *            numero da conta.
+	 * @param valor
+	 *            valor a ser creditado.
+	 * @throws RepositorioException
+	 *             lancada caso ocorra erro com a persistencia.
+	 * @throws ContaNaoEncontradaException
+	 *             lancada caso a conta nao exista.
+	 */
+	void creditar(String numero, double valor) throws RepositorioException,
+			ContaNaoEncontradaException;
 
-	public double getSaldo(String numero) throws RepositorioException, ContaNaoEncontradaException;
+	/**
+	 * Debita valor de uma conta do banco.
+	 * 
+	 * @param numero
+	 *            numero da conta.
+	 * @param valor
+	 *            valor a ser debitado.
+	 * @throws RepositorioException
+	 *             lancada caso ocorra erro com a persistencia.
+	 * @throws ContaNaoEncontradaException
+	 *             lancada caso a conta nao exista.
+	 * @throws SaldoInsuficienteException
+	 *             lancada caso nao existe saldo suficiente.
+	 */
+	void debitar(String numero, double valor) throws RepositorioException,
+			ContaNaoEncontradaException, SaldoInsuficienteException;
 
-	public void transferir(String de, String para, double valor) throws RepositorioException, ContaNaoEncontradaException, SaldoInsuficienteException;
-	
-	public void renderJuros(String numero) throws RepositorioException, ContaNaoEncontradaException, RenderJurosPoupancaException;
+	/**
+	 * Le o saldo de uma conta.
+	 * 
+	 * @param numero
+	 *            numero da conta.
+	 * @return valor do saldo.
+	 * @throws RepositorioException
+	 *             lancada caso ocorra erro com a persistencia.
+	 * @throws ContaNaoEncontradaException
+	 *             lancada caso a conta nao exista.
+	 */
+	double getSaldo(String numero) throws RepositorioException,
+			ContaNaoEncontradaException;
 
-	public void renderBonus(String numero) throws RepositorioException, ContaNaoEncontradaException, RenderBonusContaEspecialException;
+	/**
+	 * Transfere valor de uma conta origem para uma conta destino.
+	 * 
+	 * @param de
+	 *            numero da conta de origem.
+	 * @param para
+	 *            numero da conta de destino.
+	 * @param valor
+	 *            valor a ser transferido.
+	 * @throws RepositorioException
+	 *             lancada caso ocorra erro com a persistencia.
+	 * @throws ContaNaoEncontradaException
+	 *             lancada caso a conta nao exista.
+	 * @throws SaldoInsuficienteException
+	 *             lancada caso nao existe saldo suficiente.
+	 */
+	void transferir(String de, String para, double valor)
+			throws RepositorioException, ContaNaoEncontradaException,
+			SaldoInsuficienteException;
+
+	/**
+	 * Rende juros na conta de poupanca indicada.
+	 * 
+	 * @param numero
+	 *            numero da poupanca.
+	 * @throws RepositorioException
+	 *             lancada caso ocorra erro com a persistencia.
+	 * @throws ContaNaoEncontradaException
+	 *             lancada caso a conta nao exista.
+	 * @throws RenderJurosPoupancaException
+	 *             lancada caso o numero nao pertence a uma conta poupanca.
+	 */
+	void renderJuros(String numero) throws RepositorioException,
+			ContaNaoEncontradaException, RenderJurosPoupancaException;
+
+	/**
+	 * Rende bonus na conta especial indicada.
+	 * 
+	 * @param numero
+	 *            numero da conta especial.
+	 * @throws RepositorioException
+	 *             lancada caso ocorra erro com a persistencia.
+	 * @throws ContaNaoEncontradaException
+	 *             lancada caso a conta nao exista.
+	 * @throws RenderBonusContaEspecialException
+	 *             lancada caso o numero nao pertence a uma conta especial.
+	 */
+	void renderBonus(String numero) throws RepositorioException,
+			ContaNaoEncontradaException, RenderBonusContaEspecialException;
 }
